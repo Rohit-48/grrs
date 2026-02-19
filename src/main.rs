c00109ae66fd1022e7237745a48f3ead41fc43da
Rohit <rohitmandavkar3577@gmail.com>
2026-02-19 13:05:14 +0530

    implement clap and parser that will take cli agrs or flags to parsr them, fix some nix stuff like rust path and all.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
-fn main() {
-    println!("Hello, world!");
-    
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    // pattern to look for, as a str.
+    pattern: String,
+    // path to read file
+    path: std::path::PathBuf,
 }
 
 
+fn main() {
+    // let pattern = std::env::args().nth(1).expect("no pattern given");
+    // let path = std::env::args().nth(2).expect("no path given");
+
+    let args = Cli::parse();
+
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
+}
 
