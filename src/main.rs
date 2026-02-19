a10594272f288af0ba1e46aba56819ff5ec52684
Rohit <rohitmandavkar3577@gmail.com>
2026-02-19 13:17:29 +0530

    frist implementation of grrs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 
+
 #[derive(Parser)]
 struct Cli {
     // pattern to look for, as a str.
@@ -8,13 +9,16 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-
 fn main() {
     // let pattern = std::env::args().nth(1).expect("no pattern given");
     // let path = std::env::args().nth(2).expect("no path given");
 
     let args = Cli::parse();
 
-    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
-}
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
 
+    for line in content.lines() {
+        if line.contains(&args.pattern) {}
+        println!("{}", line)
+    }
+}
